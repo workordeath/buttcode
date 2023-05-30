b132aa550bb1011871b29c4d926997bbab1ff7df
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-05-29 20:51:20 -0400

    objoriented

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-def main():
+
+def setMinutes():
     user_input = input("Enter the amount of minutes you worked.")
     if user_input % 5 is not 0:
         raise Exception("Invalid time.")
-    
+
+def main()
+    setMinutes()
 main()

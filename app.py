58f1d7345b8ca37536603238f5ae648b6d0d9a61
Alex Arguelles <alexarguelles87923@gmail.com>
2023-05-29 20:43:11 -0400

    refa

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 def main():
     user_input = input("Enter the amount of minutes you worked.")
-    user_input % 5
+    if user_input % 5 is not 0:
+        raise Exception("Invalid time.")
     
 main()

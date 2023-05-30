307f3e457e3dd17a13e28ca8c0c689971986fb62
Alex Arguelles <alexarguelles87923@gmail.com>
2023-05-29 20:57:49 -0400

    dawd

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 def getHours():
     user_input = input("Enter the amount of minutes you worked.")
-    if user_input % 5 is not 0:
+    if user_input % 5 != 0 or user_input <= 0:
         raise Exception("Invalid time.")
     return user_input / 60
 

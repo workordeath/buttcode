739bd2ae4cd587372d5a905d62d620343d28df0f
Alex Arguelles <alexarguelles87923@gmail.com>
2023-05-29 20:54:28 -0400

    hours

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 
-def setMinutes():
+def getHours():
     user_input = input("Enter the amount of minutes you worked.")
     if user_input % 5 is not 0:
         raise Exception("Invalid time.")
+    return user_input / 60
 
-def main()
-    setMinutes()
+def main():
+    hours = getHours()
+    
 main()
